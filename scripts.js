b5e9d85eaac4ac4362525e9766f9439cb32797ef
clickchak <89925777+ClickChak@users.noreply.github.com>
2023-08-23 09:03:36 +0545

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,3 +10,20 @@ document.addEventListener('click', (event) => {
         verticalNav.classList.remove('active');
     }
 });
+// Autoscroll function
+function autoScroll() {
+  const scrollStep = 5; // Number of pixels to scroll in each step
+  const scrollInterval = 10; // Milliseconds between each scroll step
+
+  const scrollIntervalId = setInterval(function() {
+    window.scrollBy(0, scrollStep);
+
+    // Check if we've reached the bottom of the page
+    if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+      clearInterval(scrollIntervalId);
+    }
+  }, scrollInterval);
+}
+
+// Start autoscrolling when the page loads
+window.addEventListener('load', autoScroll);
