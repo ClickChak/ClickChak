8cd23c3e82d3303a32dc7056e130e4952776010e
clickchak <89925777+ClickChak@users.noreply.github.com>
2023-08-26 11:29:01 +0545

    Update scripted.js

--- scripted.js
+++ scripted.js
@@ -1,32 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const autoClickBtn = document.querySelector('.auto-click-btn');
-    const adElements = document.querySelectorAll('.ad-element');
+    const startAutoClickBtn = document.getElementById('startAutoClick');
 
     let autoClickEnabled = false;
-    let currentAdIndex = 0;
 
-    autoClickBtn.addEventListener('click', function () {
+    startAutoClickBtn.addEventListener('click', function () {
         autoClickEnabled = !autoClickEnabled;
 
         if (autoClickEnabled) {
-            autoClickBtn.textContent = 'Disable Auto-Click';
-            autoClick();
+            startAutoClickBtn.textContent = 'Stop Auto-Click';
+            observeAds();
         } else {
-            autoClickBtn.textContent = 'Enable Auto-Click';
+            startAutoClickBtn.textContent = 'Start Auto-Click';
         }
     });
 
-    function autoClick() {
-        if (autoClickEnabled) {
-            if (currentAdIndex < adElements.length) {
-                const adElement = adElements[currentAdIndex];
-                adElement.click();
-                currentAdIndex++;
-                setTimeout(autoClick, 3000); // Click every 3 seconds (adjust as needed)
-            } else {
-                autoClickEnabled = false;
-                autoClickBtn.textContent = 'Enable Auto-Click';
-            }
-        }
+    function observeAds() {
+        const adContainer = document.querySelector('.ad-container');
+
+        if (!adContainer) return;
+
+        const observer = new MutationObserver(mutations => {
+            mutations.forEach(mutation => {
+                if (mutation.type === 'childList') {
+                    const newAds = Array.from(mutation.addedNodes).filter(node => node.className === 'ad-element');
+                    newAds.forEach(ad => {
+                        ad.click();
+                    });
+                }
+            });
+        });
+
+        const config = { childList: true, subtree: true };
+        observer.observe(adContainer, config);
     }
 });
