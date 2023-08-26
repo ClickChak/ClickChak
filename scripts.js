84ed049c5f365a994d8644e148f686bb61057bae
clickchak <89925777+ClickChak@users.noreply.github.com>
2023-08-26 10:17:45 +0545

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -27,32 +27,3 @@ function autoScroll() {
 
 // Start autoscrolling when the page loads
 window.addEventListener('load', autoScroll);
-
-document.addEventListener('DOMContentLoaded', function () {
-    const autoClickBtn = document.querySelector('.auto-click-btn');
-    const blankArea = document.querySelector('.blank-area');
-
-    let autoClickEnabled = false;
-
-    autoClickBtn.addEventListener('click', function () {
-        autoClickEnabled = !autoClickEnabled;
-
-        if (autoClickEnabled) {
-            autoClickBtn.textContent = 'Disable Auto-Click';
-            autoClick();
-        } else {
-            autoClickBtn.textContent = 'Enable Auto-Click';
-        }
-    });
-
-    function autoClick() {
-        if (autoClickEnabled) {
-            const newTab = window.open('https://www.advertiser.com/popunder-ad', '_blank');
-            newTab.blur();
-            window.focus();
-            
-            setTimeout(autoClick, 3000); // Click every 3 seconds (adjust as needed)
-        }
-    }
-});
-
